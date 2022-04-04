c6479bedf438ea71a1d17a075ac2d42a5af99f16
Amirkhan5678 <sh4444qtr@gmail.com>
2022-04-04 08:59:51 +0500

    js work complete

---
+++ app.js
@@ -0,0 +1,107 @@
+//CH#01//
+//QNO:01//
+
+// var arr=[];
+// console.log(arr);
+
+//QNO:02//
+
+// var arr=[0,1,2,3];
+// console.log(arr)
+// var arr1=[1,0,2,1];
+// console.log(arr1)
+// var arr2=[2,1,3,0];
+// console.log(arr2)
+
+//QNO:03//
+
+
+// for(var i=1; i<=10; i++){
+//     document.write(i+"<br />");
+
+// }
+
+//QNO:04//
+
+// var a=+prompt("any number");
+// for(var i=+prompt("any value");i<=10;i++){
+//     document.write(a + "x" + i + "=" + i*a + "<br />")
+// }
+
+//QNO:05//
+
+// for(var i=1 ; i<=15 ; i++){
+//     document.write(2 + "x" + i + "=" + i*2 + "<br />")
+// }
+
+//QNO:06//
+
+// var arr=["apple","mango","banana","strawberry"];
+
+// for(var i=1; i<5 ; i++){
+//     document.write(arr[0]+"<br / >"+ arr[1] +"<br / >"+ arr[2] +"<br / >"+ arr[3])
+    
+
+// }
+
+//QNO:07//
+
+// for(var i=1; i=15 ; i++){
+//     document.write("Counting <br />" + i)
+
+// }
+
+
+// for(var i=1; i=15 ; i--){
+//     document.write("Counting <br />" + i)
+
+// }
+
+//QNO:08//
+
+// var arr=["cake", "apple pie","cookie","chips"]
+// var a=prompt("enter name")
+// if(a===arr){
+//     alert("Welcome to backery")
+    
+// }
+// else{
+//     alert(we are sorry )
+// }
+
+///array.//
+ ///QNO:01///
+  
+//  var arr=[];
+// console.log(arr)
+
+///QNO:02///
+
+//  var student name=[];
+// console.log(arr)
+
+///QNO:03///
+//  var string=["enter any string"];
+//  console.log(string)
+
+///QNO:04///
+
+// var number=[1,2,3,4];
+// console.log(number)
+
+///QNO:05///
+
+// var mixed=[1,2,3,"strawberry"];
+// console.log(mixed);
+
+///QNO:06///
+//    var education=["ssc ","hsc","fcs","bsc","msc","M.phil","PhD"];
+//    console.log(education)
+//    document.write("ssc <br />","hsc<br />","fcs<br />","bsc<br />","msc<br />","M.phil<br />","PhD") 
+
+///QNO:07///
+
+// var arr=["james","micheal","holder"];
+// var arr1=[230,250,300]
+// var result=(arr1[0]*100/500)
+// document.write(arr[0] +arr1[0]  +"<br />" + arr[1] + arr1[1] + "<br />" + arr[2] +arr1[2])
